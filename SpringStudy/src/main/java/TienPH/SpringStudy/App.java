3296cd254a4f97d14efad06e3342e76d79f9278b
hoangtien.cp <hoangtien.cp@gmail.com>
2017-07-22 14:54:58 +0700

    abc

---
+++ SpringStudy/src/main/java/TienPH/SpringStudy/App.java
@@ -0,0 +1,13 @@
+package TienPH.SpringStudy;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
